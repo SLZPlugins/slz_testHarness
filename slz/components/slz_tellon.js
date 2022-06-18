dde3bbe167fce26948af0821a41806c60ca0a58e
ZachSinger <32113115+ZachSinger@users.noreply.github.com>
2022-06-17 23:16:42 -0400

    Added sub class and child class for log records (#47)

--- slz/components/slz_tellon.js
+++ slz/components/slz_tellon.js
@@ -16,7 +16,69 @@ class TellonReporter {
     }
 }
 
+TellonReporter.setStandardProps = function(){
+    this._reports = [];
+}
 
-slz_Harness.addafterAllTestHook(()=>{console.log('Hi I am Tellon and I am telling on you!')})
+TellonReporter.createReport = function(){
+    this._reports.push(new TellonReport(slz_Harness.logger.getLastTestRunLogs()))
+    this.printLastReport()
+}
+
+TellonReporter.printLastReport = function(){
+    this._reports[this._reports.length - 1].generateReport()
+    this._reports[this._reports.length - 1].print()
+}
+
+TellonReporter.printAllReports = function(){
+    this._reports.forEach(report => {
+        report.generateReport()
+        report.print()
+    })
+}
+
+
+
+class TellonReport {
+
+    constructor(logs){
+        this.logs = logs;
+        this.reportString = ""
+    }
+
+    generateReport(){
+        let list = this.logs;
+        let length = list.length;
+        let current;
+
+        for(let i = 0; i < length; i++){
+            current = list[i];
+            if(current instanceof slz_AssertionRecord){
+                if(current.isPassing){
+                    this.reportString += '\tPASS'
+                    this.reportString += '\n'
+                } else {
+                    this.reportString += '\tFAIL'
+                    this.reportString += '\n'
+                    this.reportString += current.toString()
+                }
+
+                this.reportString += '\n'
+            } else {
+                this.reportString += '\n' + current.toString()
+            }
+        }
+
+    }
+
+    print(){
+        console.log(this.reportString)
+    }   
+
+}
+
+TellonReporter.setStandardProps()
+
+slz_Harness.addafterAllTestHook(TellonReporter.createReport.bind(TellonReporter))
 
 slz_Harness.registerModule('tellon')
